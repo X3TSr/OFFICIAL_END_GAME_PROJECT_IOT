fba83b5e0b41bef6503378d68b05666e43873af7
X3TSr <kylian1203@gmail.com>
2024-05-15 11:48:47 +0200

    More C0de

--- Controller.py
+++ Controller.py
@@ -8,8 +8,40 @@ TYPES = ['rest', 'pmd', 'karton/papier', 'gft', 'kga']
 
 for type in TYPES:
     if response.lower() == type:
-        print(type)
-        motor.forwards()
-        time.sleep(3)
-        motor.stop()
-        # if type == TYPES[0]:
+        if type == TYPES[0]:
+            print(type)
+            motor.forwards()
+            time.sleep(3)
+            motor.backwards()
+            time.sleep(3)
+            motor.stop()
+        elif type == TYPES[1]:
+            print(type)
+            motor.forwards()
+            time.sleep(3)
+            motor.backwards()
+            time.sleep(3)
+            motor.stop()
+        elif type == TYPES[2]:
+            print(type)
+            motor.forwards()
+            time.sleep(3)
+            motor.backwards()
+            time.sleep(3)
+            motor.stop()
+        elif type == TYPES[3]:
+            print(type)
+            motor.forwards()
+            time.sleep(3)
+            motor.backwards()
+            time.sleep(3)
+            motor.stop()
+        elif type == TYPES[4]:
+            print(type)
+            motor.forwards()
+            time.sleep(3)
+            motor.backwards()
+            time.sleep(3)
+            motor.stop()
+        else:
+            print('ERROR')
